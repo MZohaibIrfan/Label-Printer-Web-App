7bd5a0474ceddf85d906d12f3a08a6a0e6ca96a5
Muhammad Zohaib <75018124+MZohaibIrfan@users.noreply.github.com>
2025-01-07 09:57:24 +0800

    Add files via upload

--- label/App.js
+++ label/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import CheckoutPage from './pages/CheckoutPage';
-import OrderPage from './pages/OrderPage';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import CheckoutPage from './pages/CheckoutPage';
+import OrderPage from './pages/OrderPage';
 import WaiterPage from './pages/WaiterPage';
 import CashierPage from './pages/CashierPage';
 import OrderHistory from './pages/OrderHistory';
-import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
 import { CartProvider } from './contexts/CartContext';
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
-  const [userRole, setUserRole] = React.useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userRole, setUserRole] = useState('');
 
-  React.useEffect(() => {
+  useEffect(() => {
     const token = localStorage.getItem('token');
     const role = localStorage.getItem('role');
     if (token && role) {
@@ -35,11 +35,10 @@ function App() {
           <Route path="/login" element={<LoginPage setIsAuthenticated={setIsAuthenticated} setUserRole={setUserRole} />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/checkout" element={<CheckoutPage setIsAuthenticated={setIsAuthenticated} />} />
-          <Route path="/order" element={isAuthenticated ? <OrderPage /> : <Navigate to="/login" />} />
+          <Route path="/home" element={<HomePage setIsAuthenticated={setIsAuthenticated} />} /> {/* Allow access to HomePage without authentication */}
           <Route path="/waiter-portal" element={isAuthenticated && userRole === 'waiter' ? <WaiterPage setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
           <Route path="/cashier-portal" element={isAuthenticated && userRole === 'cashier' ? <CashierPage setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
           <Route path="/order-history" element={isAuthenticated && userRole === 'customer' ? <OrderHistory setIsAuthenticated={setIsAuthenticated} /> : <Navigate to="/login" />} />
-          <Route path="/home" element={<HomePage setIsAuthenticated={setIsAuthenticated} />} />
           <Route path="/" element={<Navigate to="/home" />} />
         </Routes>
       </Router>
